0df58131a4505dc07bacf9211c40e57fbc4141ef
Umarkhon Akhmedov <umarkhonakhmedov2000@gmail.com>
2024-02-06 07:54:59 +0500

    Adding Navbat and Store Switcher for dashboard

--- ecommerce-admin/components/modals/store-modal.tsx
+++ ecommerce-admin/components/modals/store-modal.tsx
@@ -39,7 +39,7 @@ export const StoreModal = () => {
       setLoading(true);
       const response = await axios.post("/api/stores", values);
 
-      console.log(response.data);
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("Something went wrong")
     } finally {
